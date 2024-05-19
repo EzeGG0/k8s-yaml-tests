519bb6602aa83fc060b18fd47e224cc28fc6f3cb
Eze <94138154+EzeGG0@users.noreply.github.com>
2024-05-18 21:41:57 -0300

    Problemas de Back y Front solucionados. Falta hacer los deploy y services de Kubernetes

--- test/backend/demo/src/main/java/com/example/demo/config/SecurityConfig.java
+++ test/backend/demo/src/main/java/com/example/demo/config/SecurityConfig.java
@@ -1,25 +1,25 @@
-// package com.example.demo.config;
-// import org.springframework.context.annotation.Bean;
-// import org.springframework.context.annotation.Configuration;
-// import org.springframework.security.config.annotation.web.builders.HttpSecurity;
-// import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
-// import org.springframework.security.web.SecurityFilterChain;
+package com.example.demo.config;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
+import org.springframework.security.web.SecurityFilterChain;
 
-// @Configuration
-// @EnableWebSecurity
-// public class SecurityConfig {
+@Configuration
+@EnableWebSecurity
+public class SecurityConfig {
 
-//     @Bean
-//     public SecurityFilterChain filterChain(HttpSecurity http) throws Exception {
-//         http
-//                 .authorizeHttpRequests(authorize -> authorize
-//                 .anyRequest().permitAll()
-//                 )
-//                 .cors(cors -> cors.disable()
-//                 )
-//                 .csrf(csrf -> csrf.disable())
-//                 .formLogin(login -> login.disable());
+    @Bean
+    public SecurityFilterChain filterChain(HttpSecurity http) throws Exception {
+        http
+                .authorizeHttpRequests(authorize -> authorize
+                .anyRequest().permitAll()
+                )
+                .cors(cors -> cors.disable()
+                )
+                .csrf(csrf -> csrf.disable())
+                .formLogin(login -> login.disable());
 
-//         return http.build();
-//     }
-// }
+        return http.build();
+    }
+}
--- test/backend/demo/src/main/java/com/example/demo/config/WebConfiguration.java
+++ test/backend/demo/src/main/java/com/example/demo/config/WebConfiguration.java
@@ -1,14 +1,14 @@
-// package com.example.demo.config;
+package com.example.demo.config;
 
-// import org.springframework.context.annotation.Configuration;
-// import org.springframework.web.servlet.config.annotation.CorsRegistry;
-// import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.web.servlet.config.annotation.CorsRegistry;
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
 
-// @Configuration
-// public class WebConfiguration implements WebMvcConfigurer {
+@Configuration
+public class WebConfiguration implements WebMvcConfigurer {
 
-//     @Override
-//     public void addCorsMappings(CorsRegistry registry) {
-//         registry.addMapping("/**").allowedMethods("*");
-//     }
-// }
+    @Override
+    public void addCorsMappings(CorsRegistry registry) {
+        registry.addMapping("/**").allowedMethods("*");
+    }
+}
